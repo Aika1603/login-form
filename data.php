021f1b0cba9a04b4c3e28f2441af6ee5ecd2873a
asep <asep.saepul205@gmail.com>
2020-05-04 21:12:08 +0700

    update

---
+++ data.php
@@ -0,0 +1,7 @@
+<?php
+$response = array(
+    'status' => 20,
+    'message' => 'Silahkan Tunggu - Anda sedang dialihkan',
+    'return_url' => 'http://localhost/form-login'
+    );
+echo json_encode($response);
--- js/main.js
+++ js/main.js
@@ -42,76 +42,122 @@ $('.btn-forgot').click(function (params) {
 	$('#img-bg').attr('src', 'img/undraw_my_password_d6kg.svg');
 });
 
+
 $('.form-ajax').submit(function (e) {
+	$('#render-toast').html('');
+
 	$('input[type=submit]').prop('disabled', true);
-	$('.loading').toggle();
+	//loading toggle
+	let param = {
+		img: "",
+		title: "Validasi Login",
+		message: '<i class="fa fa-spinner fa-spin" ></i> Mengecek Akun - Silahkan Tunggu',
+		autohide: 'false',
+	}
+	$('#render-toast').append(createToast(param));
+	$('.toast').toast('show');
+	
 	$.ajax({
 		type: 'POST',
 		url: $(this).attr('action'),
 		data: new FormData($(this)[0]),
 		success: function (data) {
+
+			//close loading
+			$('#render-toast').html('');
+
 			data = JSON.parse(data);
 			if (data.status == 20) {
-				swal("Success", data.message, "success").then((value) => {
+				let param = {
+					img: "",
+					title: "Login Berhasil",
+					message: data.message,
+					autohide: 'true',
+					delay: '2000'
+				}
+				$('#render-toast').append(createToast(param));
+				$('.toast').toast('show').on('hide.bs.toast', function () {
 					if (data.return_url != '#') {
 						document.location.href = data.return_url
 					}
-				});
+				})
 			} else {
-				swal("Failed", data.message, "error");
+				let param = {
+					img: "",
+					title: "Login Gagal",
+					message: data.message,
+					autohide: 'true',
+					delay: '2000'
+				}
+				$('#render-toast').append(createToast(param));
+				$('.toast').toast('show');
 			}
-			$('.loading').toggle();
 			$('input[type=submit]').prop('disabled', false);
+
 		},
 		cache: false,
 		contentType: false,
 		processData: false,
 		error: function (data) {
-			swal(data);
-			$('.loading').toggle();
+
+			//close loading
+			$('#render-toast').html('');
+
+			// swal(data);
+			let param = {
+				img: "",
+				title: "Login Gagal",
+				message: data,
+				autohide: 'true',
+				delay: '2000'
+			}
+			$('#render-toast').append(createToast(param));
+			$('.toast').toast('show');
+
 			$('input[type=submit]').prop('disabled', false);
+			
 		}
 	});
 	e.preventDefault();
+
 });
 
-//FOR TOAST
+//testing toast
+//for yesterday. try to add background color
 $(document).ready(function () {
-	let param = {
-		img: "",
-		title: "Doe",
-		time: 'sejam yang lalu',
-		message: 'pesan ada disini nih',
-		autohide:  'false',
-	}
-	$('#render-toast').append(createToast(param));
-	 param = {
-		img: "",
-		title: "dawda",
-		time: 'sejam yang lalu',
-		message: 'pesan ada disini nih',
-		autohide: 'false',
-	}
-	$('#render-toast').append(createToast(param));
-	$('.toast').toast('show');
+	// let param = {
+	// 	img: "",
+	// 	title: "Doe",
+	// 	time: 'sejam yang lalu',
+	// 	message: 'pesan ada disini nih',
+	// 	autohide:  'false',
+	// }
+	// $('#render-toast').append(createToast(param));
+	// let  param = {
+	// 	img: "",
+	// 	title: "Validasi Login",
+	// 	time: 'sekarang',
+	// 	message: '<i class="fa fa-spinner fa-spin" ></i> Mengecek Akun - Silahkan Tunggu',
+	// 	autohide: 'false',
+	// }
+	// $('#render-toast').append(createToast(param));
+	// $('.toast').toast('show');
+	// setTimeout(function () {
+	// 	//do what you need here
+	// 	if (param.img == "") {
+	// 		$('.toast').toast('hide');
+	// 	}
+	// }, 2000);
 });
 
 function createToast(param){
-
-	const { img, title, time, message, autohide } = param;
-
-	let Image = '';
-	if (img == ''){
-		Image = `<img src="img/avatar.svg" class="rounded mr-2 img-20" alt="...">`;
-	}
-	let html = `<div class="toast animated bounceIn" role="alert" aria-live="assertive"  aria-atomic="true" style="width:300px;" data-autohide="${autohide}">
-				<div class="toast-header">
-					${Image}
+	const { img = '<img src="img/avatar.svg" class="rounded mr-2 img-20" alt="...">', title = 'title', time = '', message = 'test', autohide = 'true', delay = '10000'} = param;
+	const html = `<div class="toast" role="alert" aria-live="assertive"  aria-atomic="true" style="width:300px;" data-autohide="${autohide}" data-delay="${delay}">
+				<div class="toast-header" >
+					${img}
 					<strong class="mr-auto">${title}</strong>
 					<small class="text-muted">${time}</small>
-					<button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
+						<i class="fa fa-times ml-2  mb-1 close" style="font-size:20px;" data-dismiss="toast" aria-label="Close"></i>
 				</div>
 				<div class="toast-body">
 					${message}
